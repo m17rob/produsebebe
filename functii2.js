a6909bd12c39082d6c18960d7da8002fd05946e2
RTweety <mailspam693@gmail.com>
2023-03-02 03:01:14 +0200

    menumobile

--- functii2.js
+++ functii2.js
@@ -1,9 +1,32 @@
-const menuBtn = document.querySelector(".meniu");
+const menuBtn = document.querySelector(".meniu__btn");
+const menu = document.querySelector(".meniu");
+
+if(window.innerWidth < 910){
+    menu.classList.add("closed");
+    menuBtn.setAttribute('src', '/img/meniu-close.png')
+}
 
 menuBtn.addEventListener("click", function(){
-    if(this.classList.contains("closed")){
-        this.classList.remove("closed")
+    if(menu.classList.contains("closed")){
+        menu.classList.remove("closed")
     } else {
-        this.classList.add('closed');
+        menu.classList.add('closed');
     }
+
+    if(menu.classList.contains('closed')){
+        menuBtn.setAttribute('src', '/img/meniu-close.png')
+    } else {
+        menuBtn.setAttribute('src', '/img/meniu-open.png')
+    }
+})
+/////////////////////////////////////////////////////////
+const menuCategorii = document.querySelectorAll(".meniu__categorie");
+
+menuCategorii.forEach(function(btn){
+    btn.addEventListener("click", function(){
+        menuCategorii.forEach(function(link){
+            link.classList.remove("active")
+        });
+        this.classList.add('active');
+    })
 })
