f987029407c3b94c108d3dd20cfa4c9583263710
RTweety <mailspam693@gmail.com>
2023-03-02 03:15:00 +0200

    menumobile

--- functii2.js
+++ functii2.js
@@ -1,24 +1,24 @@
 const menuBtn = document.querySelector(".meniu__btn");
 const menu = document.querySelector(".meniu");
 
-// if(window.innerWidth < 910){
-//     menu.classList.add("closed");
-//     menuBtn.setAttribute('src', '/img/meniu-close.png')
-// }
+if(window.innerWidth < 910){
+    menu.classList.add("closed");
+    menuBtn.setAttribute('src', '/img/meniu-close.png')
+}
 
-// menu.addEventListener("click", function(){
-//     if(menu.classList.contains("closed")){
-//         menu.classList.remove("closed")
-//     } else {
-//         menu.classList.add('closed');
-//     }
+menu.addEventListener("click", function(){
+    if(menu.classList.contains("closed")){
+        menu.classList.remove("closed")
+    } else {
+        menu.classList.add('closed');
+    }
 
-    // if(menu.classList.contains('closed')){
-    //     menuBtn.setAttribute('src', '/img/meniu-close.png')
-    // } else {
-    //     menuBtn.setAttribute('src', '/img/meniu-open.png')
-    // }
-// })
+    if(menu.classList.contains('closed')){
+        menuBtn.setAttribute('src', '/img/meniu-close.png')
+    } else {
+        menuBtn.setAttribute('src', '/img/meniu-open.png')
+    }
+})
 /////////////////////////////////////////////////////////
 const menuCategorii = document.querySelectorAll(".meniu__categorie");
 
